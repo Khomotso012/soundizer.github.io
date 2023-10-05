804d97a165b24d7efb0b6f95c4ba33f45a77d380
Khomotso <110993313+Khomotso012@users.noreply.github.com>
2023-10-05 11:30:28 +0200

    code

---
+++ Library.js
@@ -0,0 +1,186 @@
+﻿console.log("Welcome to Spotify");
+
+// Initialize the Variables
+let songIndex = 0;
+let audioElement = new Audio('songs/1.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let ProgressBar = document.getElementById('ProgressBar');
+let gif = document.getElementById('gif');
+let masterSongName = document.getElementById('masterSongName');
+let songItems = Array.from(document.getElementsByClassName('songitem'));
+
+let songs = [
+    { songName: "15 HOURS", filePath: "songs/1.mp3", coverPath: "covers/3.jpg" },
+    { songName: "Amagents", filePath: "songs/2.mp3", coverPath: "covers/3.jpg" },
+    { songName: "Be There For Me", filePath: "songs/3.mp3", coverPath: "covers/3.jpg" },
+    { songName: "Dlala", filePath: "songs/4.mp3", coverPath: "covers/3.jpg" },
+
+    { songName: "Take It There", filePath: "songs/43.mp3", coverPath: "covers/VTT.jpg" },
+    { songName: "Pull Out", filePath: "songs/52.mp3", coverPath: "covers/VTT.jpg" },
+    { songName: "Express Yourself", filePath: "songs/56.mp3", coverPath: "covers/VTT.jpg" },
+    { songName: "The Girl She Was", filePath: "songs/4.mp3", coverPath: "covers/VTT.jpg" },
+
+    { songName: "Blowin Mines", filePath: "songs/44.mp3", coverPath: "covers/tlf.jpg" },
+    { songName: "Flex", filePath: "songs/46.mp3", coverPath: "covers/tlf.jpg" },
+    { songName: "To Dream", filePath: "songs/51.mp3", coverPath: "covers/tlf.jpg" },
+    { songName: "Paid The Fine", filePath: "songs/61.mp3", coverPath: "covers/tlf.jpg" },
+
+    { songName: "Zodiac", filePath: "songs/65.mp3", coverPath: "covers/dhlt.jpg" },
+    { songName: "Like That", filePath: "songs/64.mp3", coverPath: "covers/dhlt.jpg" },
+    { songName: "Pinot Noir", filePath: "songs/66.mp3", coverPath: "covers/dhlt.jpg" },
+
+    { songName: "Wharf Talk", filePath: "songs/62.mp3", coverPath: "covers/TTC.png" },
+    { songName: "Heaven To Me", filePath: "songs/63.mp3", coverPath: "covers/TTC.png" },
+    { songName: "LWeston Roads", filePath: "songs/14.mp3", coverPath: "covers/TTC.png" },
+
+    { songName: "How Much A Dolla Cost", filePath: "songs/21.mp3", coverPath: "covers/KLTPPP.png" },
+    { songName: "Mortal Man", filePath: "songs/45.mp3", coverPath: "covers/KLTPPP.png" },
+    { songName: "60 Days", filePath: "songs/55.mp3", coverPath: "covers/KLTPPP.png" },
+
+    { songName: "Wont He Do It", filePath: "songs/13.mp3", coverPath: "covers/cw.png" },
+    { songName: "Water To Wine", filePath: "songs/8.mp3", coverPath: "covers/cw.png" },
+    { songName: "Lovely", filePath: "songs/48.mp3", coverPath: "covers/cw.png" },
+
+    { songName: "Spirited Away", filePath: "songs/5.mp3", coverPath: "covers/bsa.jpg" },
+    { songName: "FataL Attraction", filePath: "songs/6.mp3", coverPath: "covers/bsa.jpg" },
+    { songName: "Dont Let Me Down", filePath: "songs/30.mp3", coverPath: "covers/bsa.jpg" },
+
+    { songName: "IFYKYK", filePath: "songs/70.mp3", coverPath: "covers/av.jpg" },
+    { songName: "So Be It", filePath: "songs/69.mp3", coverPath: "covers/av.jpg" },
+
+    { songName: "ODEE", filePath: "songs/39.mp3", coverPath: "covers/BOO.jpg" },
+    { songName: "DIAMONDS & FREAKS", filePath: "songs/53.mp3", coverPath: "covers/BOO.jpg" },
+
+    { songName: "Palm Tree", filePath: "songs/40.mp3", coverPath: "covers/jhm.jpg" },
+    { songName: "Masculine", filePath: "songs/39.mp3", coverPath: "covers/jhm.jpg" },
+
+    { songName: "Hard 2 Forget", filePath: "songs/42.mp3", coverPath: "covers/ptg.png" },
+    { songName: "The Girl She was", filePath: "songs/70.mp3", coverPath: "covers/ptg.png" },
+
+    { songName: "Don't Mind Me", filePath: "songs/37.mp3", coverPath: "covers/rwdmm.jpg" },
+    { songName: "You aint in the 6", filePath: "songs/38.mp3", coverPath: "covers/rwdmm.jpg" },
+
+    { songName: "Sorry", filePath: "songs/62.mp3", coverPath: "covers/eyy.jpg" },
+    { songName: "Do You Mean", filePath: "songs/69.mp3", coverPath: "covers/eyy.jpg" },
+
+    { songName: "Blocked", filePath: "songs/25.mp3", coverPath: "covers/isca.png" },
+    { songName: "Let me go", filePath: "songs/29.mp3", coverPath: "covers/isca.png" },
+
+    { songName: "Bikhra-Abdul Hannan", filePath: "songs/3.mp3", coverPath: "covers/Bikhra - Abdul Hannan.jpg" },
+    { songName: "Harleys In Hawaii-Katy Perry", filePath: "songs/6.mp3", coverPath: "covers/Harleys In Hawaii - Katy Perry.jpg" },
+    { songName: "Heat Waves-Glass Animals", filePath: "songs/7.mp3", coverPath: "covers/Heat Waves - Glass Animals.jpg" },
+    { songName: "Hope-XXXTENTACION", filePath: "songs/8.mp3", coverPath: "covers/Hope -XXXTENTACION.jpg" },   
+    { songName: "Akhiyaan - Mitraz", filePath: "songs/14.mp3", coverPath: "covers/Akhiyaan - Mitraz.jpg" },
+    { songName: "Anjaan - Jani,Talha Yonus", filePath: "songs/15.mp3", coverPath: "covers/Anjaan-Jani.jpg" },
+    { songName: "Ashes(2020) - Stellar", filePath: "songs/16.mp3", coverPath: "covers/Ashes-Stellar.jpg" },
+    { songName: "Back To The Future-Bastille", filePath: "songs/18.mp3", coverPath: "covers/Back To The Future-Bastille.jpg" },  
+    { songName: "Burning Down - EYK", filePath: "songs/22.mp3", coverPath: "covers/Burning Down-Everyone You Know.jpg" },       
+    { songName: "Desire-Years & Years", filePath: "songs/27.mp3", coverPath: "covers/Desire-Years & Years.jpg" },
+    { songName: "El Melouk-Ahmed Saad", filePath: "songs/32.mp3", coverPath: "covers/El Melouk-Ahmed Saad.jpg" },
+    { songName: "Excuses-Ap Dhillon", filePath: "songs/33.mp3", coverPath: "covers/Excuses-Ap Dhillon.jpg" },
+    { songName: "Formula - Labrinth", filePath: "songs/34.mp3", coverPath: "covers/Formula-Labrinth.jpg" },
+    { songName: "Goosebumps-Travis Scott", filePath: "songs/35.mp3", coverPath: "covers/Goosebumps-Travis Scott.jpg" },
+    { songName: "Head First-Christian French", filePath: "songs/36.mp3", coverPath: "covers/Head First-Christian French.jpg" },
+    { songName: "Heartburn(Remix) - Wafia", filePath: "songs/37.mp3", coverPath: "covers/Heartburn-Wafia.jpg" },
+    { songName: "Honeypie - JAWNY", filePath: "songs/38.mp3", coverPath: "covers/Honeypie-JAWNY.jpg" },    
+    { songName: "Kana Yaari Kaifi Khalil", filePath: "songs/41.mp3", coverPath: "covers/Kana Yaari Kaifi Khalil.jpg" },
+    { songName: "Lights Down Low-MAX", filePath: "songs/46.mp3", coverPath: "covers/Lights Down Low-MAX.jpg" },
+    { songName: "Love Nwantiti-CKay", filePath: "songs/47.mp3", coverPath: "covers/Love Nwantiti-CKay.jpg" },
+    { songName: "Michael Jackson-Lovin You", filePath: "songs/49.mp3", coverPath: "covers/Loving You-Michael Jackson.jpg" },
+    { songName: "Maikada - M. Samie", filePath: "songs/51.mp3", coverPath: "covers/Maikada-Muhammad Samie.jpg" },
+    { songName: "Mushk OST-Ali Zafar", filePath: "songs/52.mp3", coverPath: "covers/Mushk OST-Ali Zafar.jpg" },
+    { songName: "Mystery of Love-Sufjan Stevens", filePath: "songs/54.mp3", coverPath: "covers/Mystery of Love-Sufjan Stevens.jpg" },
+    { songName: "Passive-The Perfect Circle", filePath: "songs/56.mp3", coverPath: "covers/Passive-The Perfect Circle.jpg" },
+    { songName: "Peaky Blinder-Otnicka", filePath: "songs/57.mp3", coverPath: "covers/Peaky Blinder-Otnicka.jpg" },
+    { songName: "The Hot Stepper-Ini Kamoze", filePath: "songs/66.mp3", coverPath: "covers/The Hot Stepper-Ini Kamoze.jpg" },
+    { songName: "Vibe(2021) -Herman", filePath: "songs/67.mp3", coverPath: "covers/Vibe-Herman.jpg" },
+    { songName: "World Is Mine-Samm Henshaw", filePath: "songs/71.mp3", coverPath: "covers/World Is Mine-Samm Henshaw.jpg" },
+    { songName: "Без тебя я не я-JONY", filePath: "songs/72.mp3", coverPath: "covers/Без тебя я не я-JONY.jpg" },
+    { songName: "Right Round - Flo Rida", filePath: "songs/59.mp3", coverPath: "covers/Right Round - Flo Rida.jpg" },
+]
+
+songItems.forEach((element, i) => {
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
+})
+
+
+// Handle play/pause click
+masterPlay.addEventListener('click', () => {
+    if (audioElement.paused || audioElement.currentTime <= 0) {
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity = 1;
+    }
+    else {
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+        gif.style.opacity = 0;
+    }
+})
+// Listen to Events
+audioElement.addEventListener('timeupdate', () => {
+    // Update Seekbar
+    progress = parseInt((audioElement.currentTime / audioElement.duration) * 100);
+    ProgressBar.value = progress;
+})
+
+ProgressBar.addEventListener('change', () => {
+    audioElement.currentTime = ProgressBar.value * audioElement.duration / 100;
+})
+
+const makeAllPlays = () => {
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element) => {
+        element.classList.remove('fa-pause-circle');
+        element.classList.add('fa-play-circle');
+    })
+}
+
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element) => {
+    element.addEventListener('click', (e) => {
+        makeAllPlays();
+        songIndex = parseInt(e.target.id);
+        e.target.classList.remove('fa-play-circle');
+        e.target.classList.add('fa-pause-circle');
+        audioElement.src = `songs/${songIndex + 1}.mp3`;
+        masterSongName.innerText = songs[songIndex].songName;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        gif.style.opacity = 1;
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+    })
+})
+
+document.getElementById('next').addEventListener('click', () => {
+    if (songIndex >= 71) {
+        songIndex = 0
+    }
+    else {
+        songIndex += 1;
+    }
+    audioElement.src = `songs/${songIndex + 1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+
+})
+
+document.getElementById('previous').addEventListener('click', () => {
+    if (songIndex <= 0) {
+        songIndex = 0
+    }
+    else {
+        songIndex -= 1;
+    }
+    audioElement.src = `songs/${songIndex + 1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+})
---
+++ script.js
@@ -0,0 +1,171 @@
+﻿console.log("Welcome to Spotify");
+
+// Initialize the Variables
+let songIndex = 0;
+let audioElement = new Audio('songs/1.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let ProgressBar = document.getElementById('ProgressBar');
+let gif = document.getElementById('gif');
+let masterSongName = document.getElementById('masterSongName');
+let songItems = Array.from(document.getElementsByClassName('songitem'));
+
+let songs = [
+    { songName: "15 Hours - Teekay", filePath: "songs/1.mp3", coverPath: "covers/15hours.jpg" },
+    { songName: "AmaGents - Teekay", filePath: "songs/2.mp3", coverPath: "covers/amagents.jpg" },
+    { songName: "Be There For Me - Teekay", filePath: "songs/3.mp3", coverPath: "covers/bethere.jpg" },
+    { songName: "Dlala - Teekay", filePath: "songs/4.mp3", coverPath: "covers/bethere.jpg" },
+    { songName: "Emotions - Teekay", filePath: "songs/5.mp3", coverPath: "covers/emotions.jpg" },
+    { songName: "Future Plans - Teekay", filePath: "songs/6.mp3", coverPath: "covers/fp.jpg" },
+    { songName: "How Many Times - Teekay", filePath: "songs/7.mp3", coverPath: "covers/hmt.jpg" },
+    { songName: "I Need You - Teekay", filePath: "songs/8.mp3", coverPath: "covers/inneedyou.jpg" },
+    { songName: "Late Nights - Teekay", filePath: "songs/9.mp3", coverPath: "covers/bethere.jpg" },
+    { songName: "Mgani - Teekay", filePath: "songs/10.mp3", coverPath: "covers/mgani.jpg" },
+    { songName: "Music Fellas - Teekay", filePath: "songs/11.mp3", coverPath: "covers/bethere.jpg" },
+    { songName: "Outside The Box - Teekay", filePath: "songs/12.mp3", coverPath: "covers/bethere.jpg" },
+    { songName: "Thursday - Teekay", filePath: "songs/13.mp3", coverPath: "covers/bethere.jpg" },
+    { songName: "Weston Roads - Teekay", filePath: "songs/14.mp3", coverPath: "covers/westonroads.jpg" },
+    { songName: "EMOBOYS - Sax In The House (Deeper Mix)", filePath: "songs/15.mp3", coverPath: "covers/esih.jpg" },
+    { songName: "Ty Dolla Sign & Wiz Khalifa - Brand New", filePath: "songs/16.mp3", coverPath: "covers/twbn.jpg" },
+    { songName: "Sosh Plata", filePath: "songs/17.mp3", coverPath: "covers/sosh.jpg" },
+    { songName: "Priss - On My Way (Rancido's Lisoro Mix)", filePath: "songs/18.mp3", coverPath: "covers/omw.jpg" },
+    { songName: "Hot Now ", filePath: "songs/19.mp3", coverPath: "covers/hn.jpg" },
+    { songName: "Church - BJ the Chicago Kid", filePath: "songs/20.mp3", coverPath: "covers/bjc.jpg" },
+    { songName: "Kendrick Lamar - The Heart Part 5", filePath: "songs/21.mp3", coverPath: "covers/thp5.png" },
+    { songName: "Made For 2 - Vedo", filePath: "22.mp3", coverPath: "covers/m42.png" },
+    { songName: "Chronical Deep - Left With A Smile (Original Mix)", filePath: "23.mp3", coverPath: "covers/lwas.jpg" },
+    { songName: "Adekunle Gold, Fousheé - Dior, Dior, Dior", filePath: "songs/24.mp3", coverPath: "covers/ag.png" },
+    { songName: "Adekunle Gold, Ty Dolla sign - One Woman", filePath: "songs/25.mp3", coverPath: "covers/ag.png" },
+    { songName: "Blocked - Kay Rivers", filePath: "songs/26.mp3", coverPath: "covers/krpl.jpg" },
+    { songName: "Too Much, Forgotten Flowers", filePath: "songs/27.mp3", coverPath: "covers/ff.jpg" },
+    { songName: "Mario & Sabrina Claudio - Like Her Too - Remix", filePath: "songs/28.mp3", coverPath: "covers/lh2.png" },
+    { songName: "RAIN - Hus Kingpin", filePath: "songs/29.mp3", coverPath: "covers/rhk.jpg" },
+    { songName: "Don't Think About It Too Much", filePath: "songs/30.mp3", coverPath: "covers/mhdtatm.jpg" },
+    { songName: "Coco Jones, Justin Timberlake - ICU", filePath: "songs/31.mp3", coverPath: "covers/icu.png" },
+    { songName: "Cancelled (Instrumental)", filePath: "songs/32.mp3", coverPath: "covers/klc.png"},
+    { songName: "SiR - The Recipe", filePath: "songs/33.mp3", coverPath: "covers/str.jpg" },
+    { songName: "Too Long", filePath: "songs/34.mp3", coverPath: "covers/tl.jpg" },
+    { songName: "Dende - Nightmares", filePath: "songs/35.mp3", coverPath: "covers/dn.png" },
+    { songName: "SWV - If Only You Knew", filePath: "songs/36.mp3", coverPath: "covers/swv.jpg" },
+    { songName: "Don't Mind Me - Roy Woods", filePath: "songs/37.mp3", coverPath: "covers/rwdmm.jpg" },
+    { songName: "You Ain't In The 6 - Roy Woods", filePath: "songs/38.mp3", coverPath: "covers/rwdmm.jpg" },
+    { songName: "Masculine - J Hus", filePath: "songs/39.mp3", coverPath: "covers/jhm.jpg" },
+    { songName: "Palm Tree - J Hus", filePath: "songs/40.mp3", coverPath: "covers/jhm.jpg" },
+    { songName: "Dilo Die Chenchitse", filePath: "songs/41.mp3", coverPath: "covers/wddt.jpg" },
+    { songName: "ELEVATE", filePath: "songs/42.mp3", coverPath: "covers/ae.jpg" },
+    { songName: "Take It There - VEDO", filePath: "songs/43.mp3", coverPath: "covers/VTT.jpg" },
+    { songName: "Blowin' Mine's", filePath: "songs/44.mp3", coverPath: "covers/bmtl.jpg" },
+    { songName: "Damaged (feat. Phabo)", filePath: "songs/45.mp3", coverPath: "covers/pda.jpg" },
+    { songName: "Flex - Tory Lanez", filePath: "songs/46.mp3", coverPath: "covers/tlf.jpg" },
+    { songName: "fakumean - Gunna", filePath: "songs/47.mp3", coverPath: "covers/faku.png" },
+    { songName: "ODEEE", filePath: "songs/48.mp3", coverPath: "covers/BOO.jpg" },
+    { songName: "DIAMONDS & FREAKS", filePath: "songs/49.mp3", coverPath: "covers/BOO.jpg" },
+    { songName: "Truth Is - Vedo", filePath: "songs/50.mp3", coverPath: "covers/VTT.jpg" },
+    { songName: "To Dream - Tory Lanez", filePath: "songs/51.mp3", coverPath: "covers/tlf.jpg" },
+    { songName: "Pull Out - Vedo", filePath: "songs/52.mp3", coverPath: "covers/VTT.jpg" },
+    { songName: "Bala Bala - C-Blak", filePath: "songs/53.mp3", coverPath: "covers/BB.jpg" },
+    { songName: "How Much a Dollar Cost", filePath: "songs/54.mp3", coverPath: "covers/KLTPPP.png" },
+    { songName: "Sink or Swim", filePath: "songs/55.mp3", coverPath: "covers/sow.jpg" },
+    { songName: "Express Yourself - Phabo", filePath: "songs/56.mp3", coverPath: "covers/eyy.jpg" },
+    { songName: "Mortal Man", filePath: "songs/57.mp3", coverPath: "covers/KLTPPP.png" },
+    { songName: "Spirited Away", filePath: "songs/58.mp3", coverPath: "covers/bsa.jpg" },
+    { songName: "Fatal Attraction", filePath: "songs/59.mp3", coverPath: "covers/bsa.jpg" },
+    { songName: "60 Days - Larry June", filePath: "songs/60.mp3", coverPath: "covers/60days.jpg" },
+    { songName: "Paid The Fine", filePath: "songs/61.mp3", coverPath: "covers/ytsl.png" },
+    { songName: "WHARF TALK", filePath: "songs/62.mp3", coverPath: "covers/TTC.png" },
+    { songName: "HEAVEN TO ME", filePath: "songs/63.mp3", coverPath: "covers/TTC.png" },
+    { songName: "Like That", filePath: "songs/64.mp3", coverPath: "covers/dhlt.jpg" },
+    { songName: "Zodiac", filePath: "songs/65.mp3", coverPath: "covers/dhlt.jpg" },
+    { songName: "Pinot Noir", filePath: "songs/66.mp3", coverPath: "covers/F65.jpg" },
+    { songName: "Water To Wine", filePath: "songs/67.mp3", coverPath: "covers/cw.png" },
+    { songName: "Won't He Do It", filePath: "songs/68.mp3", coverPath: "covers/cw.png" },
+    { songName: "So Be It", filePath: "songs/69.mp3", coverPath: "covers/av.jpg" },
+    { songName: "IYKYK", filePath: "songs/70.mp3", coverPath: "covers/av.jpg" },
+    { songName: "The Girl She Was", filePath: "songs/71.mp3", coverPath: "covers/ptg.png" },
+    { songName: "Hard 2 Forget", filePath: "songs/72.mp3", coverPath: "covers/ptg.png" },
+]
+
+songItems.forEach((element, i) => {
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
+})
+
+
+// Handle play/pause click
+masterPlay.addEventListener('click', () => {
+    if (audioElement.paused || audioElement.currentTime <= 0) {
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity = 1;
+    }
+    else {
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+        gif.style.opacity = 0;
+    }
+})
+// Listen to Events
+audioElement.addEventListener('timeupdate', () => {
+    // Update Seekbar
+    progress = parseInt((audioElement.currentTime / audioElement.duration) * 100);
+    ProgressBar.value = progress;
+})
+
+ProgressBar.addEventListener('change', () => {
+    audioElement.currentTime = ProgressBar.value * audioElement.duration / 100;
+})
+
+const makeAllPlays = () => {
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element) => {
+        element.classList.remove('fa-pause-circle');
+        element.classList.add('fa-play-circle');
+    })
+}
+
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element) => {
+    element.addEventListener('click', (e) => {
+        makeAllPlays();
+        songIndex = parseInt(e.target.id);
+        e.target.classList.remove('fa-play-circle');
+        e.target.classList.add('fa-pause-circle');
+        audioElement.src = `songs/${songIndex + 1}.mp3`;
+        masterSongName.innerText = songs[songIndex].songName;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        gif.style.opacity = 1;
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+    })
+})
+
+document.getElementById('next').addEventListener('click', () => {
+    if (songIndex >= 71) {
+        songIndex = 0
+    }
+    else {
+        songIndex += 1;
+    }
+    audioElement.src = `songs/${songIndex + 1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+
+})
+
+document.getElementById('previous').addEventListener('click', () => {
+    if (songIndex <= 0) {
+        songIndex = 0
+    }
+    else {
+        songIndex -= 1;
+    }
+    audioElement.src = `songs/${songIndex + 1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+})
